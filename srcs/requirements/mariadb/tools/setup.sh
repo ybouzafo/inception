12e96e1a4d74e86eeca0f918f58daeb01a2955ee
yousa <usrabouzafour22@gmail.com>
2024-05-09 15:24:29 -0400

    7 push

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,8 +1,26 @@
-service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${db_name}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${db_user}\`@'localhost' IDENTIFIED BY '${db_pwd}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${db_name}\`.* TO \`${db_user}\`@'%' IDENTIFIED BY '${db_pwd}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${db_pwd}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$db_name shutdown
-exec mysqld_safe
+#  service mariadb start;
+#  sleep 2
+
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# mysql -e "FLUSH PRIVILEGES;"
+# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# exec mysqld_safe
+
+
+
+# # CREATE DATABASE IF NOT EXISTS wordpress;
+# # CREATE USER IF NOT EXISTS 'wordpressuser'@'%' IDENTIFIED BY '1234';
+# # GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'%';
+# # FLUSH PRIVILEGES;
+#!/bin/bash
+service mariadb start 
+sleep 1
+  mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+  mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+  mariadb -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'%';"
+  mariadb -e "FLUSH PRIVILEGES;"
+  mysqladmin -u root shutdown
+  mysqld_safe --bind-address=0.0.0.0
